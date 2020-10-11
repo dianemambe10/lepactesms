e39e9c27b9908995bb8c9e4c765a5ade0aef8a29
diane <dianemambe10@gmail.com>
2020-10-11 03:37:33 +0000

    authentification2

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,16 @@ Route::get('/', function () {
 });
 
 
-Route::get('register', 'Auth\RegisterController@register');
-Route::post('register', 'Auth\RegisterController@store');
+Route::get('register', 'Authe\RegisterController@register')->name('register');
+Route::post('register', 'Authe\RegisterController@store');
+
+Route::get('login', 'Authe\LoginController@login')->name('login');
+Route::post('login', 'Authe\LoginController@authenticate');
+Route::get('logout', 'Authe\LoginController@logout')->name('logout');
+
+Route::get('home', 'Authe\LoginController@home')->name('home');
+Route::get('forget-password', 'Authe\ForgotPasswordController@getEmail');
+Route::post('forget-password', 'Authe\ForgotPasswordController@postEmail');
+
+Route::get('reset-password/{token}', 'Authe\ResetPasswordController@getPassword');
+Route::post('reset-password', 'Authe\ResetPasswordController@updatePassword');
